215193e46543284c098c5853e7f1a27d77ad2d1a
olatorera <66270040+olatorera@users.noreply.github.com>
2022-12-18 15:01:06 +0100

    api changes

--- src/script.js
+++ src/script.js
@@ -27,15 +27,6 @@ let date = new Date();
 timeSelector.innerHTML = getDate(date);
 let iconElement = document.querySelector("#icon");
 
-getForecast = (coordinates) => {
-  console.log(coordinates);
-  let apiKey = "3f4aa0abbcaff6a7e1ba0obe64d3691t";
-  let unit = "metric";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=${unit}`;
-  console.log(apiUrl, "me");
-  axios.get(apiUrl).then(displayForecast);
-};
-
 function weatherResponse(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -54,9 +45,6 @@ function weatherResponse(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  console.log(response);
-
-  getForecast(response.data.coord);
 }
 
 function search(event) {
@@ -65,7 +53,6 @@ function search(event) {
   // let searchQuery = document.querySelector("#search-query");
   // cityElement.innerHTML = searchQuery.value;
   // Making an API call once i get the response it wil be displayed
-
   let apiKey = "7b903fce3e3e1cc78f5ab959d5b024a5";
   let city = document.querySelector("#search-query").value;
   let unit = "metric";
@@ -100,31 +87,26 @@ celsuisLink.addEventListener("click", convertToCelsuis);
 
 // displaying the weather forecast multiple times
 
-const displayForecast = (response) => {
-  console.log(response.data.daily, "data");
-
-  let dailyForecast = response.data.daily;
-
+const displayForecast = () => {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
   let days = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
-
-  dailyForecast.forEach(function (forecastDay) {
+  days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       ` <div class="col">
   <div class="card each-card">
-    <h5>${forecastDay.dt}</h5>
+    <h5>${day}</h5>
     <img
       src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
       class="card-img-top"
       alt="..."
     />
     <div>
-      <span class="sec-2-text-max">${forecastDay.temp.max}</span>
-      <span class="sec-2-text-min">${forecastDay.temp.min}</span>
+      <span class="sec-2-text-max">31°</span>
+      <span class="sec-2-text-min">24°</span>
     </div>
   </div>
 </div>
@@ -136,4 +118,4 @@ const displayForecast = (response) => {
   forecastHTML = `</div>`;
 };
 
-// displayForecast();
+displayForecast();
